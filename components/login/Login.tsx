d016a34691f8352266878ea3822cbac568441da5
Maks <makzylinski@gmail.com>
2025-05-18 13:44:49 +0200

    Add login component and styles with basic functionality

---
+++ components/login/Login.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Button, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { styles } from "./LoginStyles.styles";
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLogin = () => {
+    console.log("login");
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Hasło"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <Button title="Zaloguj się" onPress={onLogin} />
+    </View>
+  );
+}
---
+++ components/login/LoginStyles.styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flex: 1,
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#aaa",
+    padding: 12,
+    marginBottom: 12,
+    borderRadius: 4,
+  },
+  error: {
+    marginBottom: 12,
+    color: "red",
+  },
+});
